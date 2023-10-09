6bee4888854d31c2026d812d31291eb310f9a52e
senesh1 <senesh@calcey.com>
2023-10-09 18:46:22 +0530

    project created

---
+++ suite-landing/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import primaryButton from './components/primaryButton'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='Heading-XL'>hiii</h1>
+      <primaryButton>hello</primaryButton>
+      <button className='btn-primary'> btn</button>
+    </>
+  )
+}
+
+export default App
